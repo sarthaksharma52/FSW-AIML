bcd9d4379bfd57fc206d92bc5bf9d5536588f788
Sarthak Sharma <sarthak.dec04@gmail.com>
2024-10-18 10:23:25 +0530

    update and adding some data in js folder

--- JSwork/index.js
+++ JSwork/index.js
@@ -1,1 +1,62 @@
-console.log("Jai Shree Ram");
+// console.log("Jai Shree Ram");
+
+// function cCompiler(){
+//     return "C compiler";
+// }
+
+// function javaCompiler(){
+//     return "java compiler";
+// }
+
+// function selectlanguage(clbk){
+//     return "you have selected "+ clbk();
+// }
+
+// const result = selectlanguage(cCompiler);
+
+// console.log(result);
+
+
+
+
+
+// function selectlanguage(language="no compiler selected"){
+
+//     let data=language;
+
+//     if(language=='c'){
+//         function cCompiler(){
+//             return "C compiler";
+//         }
+//         data = cCompiler();
+//     }
+//     if(language=='java'){
+//         function javaCompiler(){
+//             return "java compiler";
+//         }
+//         data = javaCompiler();
+//     }
+
+// return data;
+// }
+
+// const result = selectlanguage('c');
+// console.log(result);
+
+
+
+
+// console.dir(document);
+// const div = document.getElementsByClassName("parent");
+// console.dir(div);
+// div[0].innerHTML="<h2>ABES EC</h2>";
+
+
+
+const disp=document.getElementById("disp");
+console.log(disp)
+function getData(){
+    console.log("get data called");
+    disp.innerHTML="<h2>you called getData</h2>"
+}
+
